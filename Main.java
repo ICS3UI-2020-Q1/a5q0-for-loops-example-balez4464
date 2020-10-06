5d0192213dc56e263281114b43d8cfce8376c2b6
balez4464 <balez4464@wrdsb.ca>
2020-10-06 14:07:52 +0000

    created an example for for loops

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 /**
- *
- * @author 
+ *For loop example
+ * @author Zachary Balean 
  */
 public class Main {
 
@@ -9,7 +9,24 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //creates counting varibel
+    System.out.println("Beginning to count!");
+    int count = 1;
+    //creates loop
+    while(count <= 5){
+      //print it
+      System.out.println(count);
+      //add one to count
+      count = count + 1;
+    } 
+    System.out.println("That's all for now");
     
+    //creates a for loop that counts to 5
+    for(int count2 = 1; count2 <= 5; count2 = count2++){
+      //print the number
+      System.out.println(count2);
+    }
+
+
   }
 }
